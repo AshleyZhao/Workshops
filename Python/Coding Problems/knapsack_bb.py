603afe1681052a1fdeb126245623d5ec5c151988
Ashley-Zhao <Ashley.Zhao@ibm.com>
2021-04-11 16:14:11 -0500

    working on knapsack proble m

--- Python/Coding Problems/knapsack_bb.py
+++ Python/Coding Problems/knapsack_bb.py
@@ -40,61 +40,49 @@ for i in range(0, len(items)):
 
 # Helper function to find GFC
 # Same as greedy's partial items for knapsack
-def GFC(take_list):
+def GFC(items, mass=c):
     # return this value
     greed_value = 0
 
-    temp_capacity = c
-    
-    # keeps track of the take_list
-    n = 0
+    temp_capacity = mass
 
     # if we can take the whole item, we take it
     # otherwise we take whatever unit is left
 
     for item in items:
-        if item.mass <= temp_capacity and take_list[n] != 0:
+        if item.mass <= temp_capacity:
             greed_value += item.value
             temp_capacity -= item.mass
-        elif item.mass > temp_capacity and take_list[n] != 0:
+        else:
             greed_value += (item.unit_value * temp_capacity)
             temp_capacity = 0
-        else:
-            return -1
-        n += 1
     return (total_value - greed_value)
 
 # Helper function to find FFC
 # Just the greedy problem 
-def FFC(take_list):
+def FFC(items, mass=c):
     # return this value
     greed_value = 0
 
-    temp_capacity = c
-
-    n = 0
+    temp_capacity = mass
 
     # if we can take the whole item, we take it
     # otherwise we leave it
 
     for item in items:
-        if item.mass <= temp_capacity and take_list[n] != 0:
+        if item.mass <= temp_capacity:
             greed_value += item.value
             temp_capacity -= item.mass
-        elif item.mass > temp_capacity and take_list[n] == 1:
-            return -1
-
-        n += 1
 
     return (total_value - greed_value)
 
-print(GFC([None]*len(items)))
-print(FFC([None]*len(items)))
+# print(GFC(items[2:]))
+# print(FFC(items))
 
 # print(x)
 
-u_global = GFC([None] * len(items))
-l_global = FFC([None] * len(items))
+l_global = GFC(items)
+u_global = FFC(items)
 
 # print(u_global, l_global)
 root = Node([None] * len(items))
@@ -139,6 +127,8 @@ n = 1
 
 while (n < len(items)):
 
+    print(n)
+
     temp_P = []
 
     for p in P:
@@ -147,14 +137,56 @@ while (n < len(items)):
         temp_left[n] = 0
         p.left = Node(temp_left)
 
-        # We check if its lower bound is greater than u_global. 
-        
-        temp_P.append(p.left)
+        # taken_list = p.left.data[:n]
 
-        temp_right = p.data[:]
-        temp_right[n] = 1
-        p.right = Node(temp_right)
-        temp_P.append(p.right)
+        # We check if this solution is actually viable:
+        mass = p.left.data[:n+1]
+        for i in range(0,n+1):
+            mass[i] = mass[i] * items[i].mass
+
+        mass = sum(mass)
+        
+        # If the solution is actually viable, we calculate lq and uq
+        if mass <= c:
+            # We need CSF + GFC for lq
+            CSF = p.left.data[:n+1]
+            print(CSF)
+            for i in range(0,n+1):
+                CSF[i] = CSF[i] * items[i].value
+            CSF = sum(CSF)
+            print("CSF",CSF)
+
+            # print(p.left.data)
+
+            gfc = GFC(items[n+1:],c-mass)
+            ffc = FFC(items[n+1:],c-mass)
+
+            print("n+1", n+1)
+
+            print("printing items")
+            for item in items[n+1:]:
+                print(item.value)
+            print("gfc, ",gfc)
+
+            lq = CSF + gfc
+            uq = CSF + ffc
+
+            print("lower bound",lq)
+            print("upper bound",uq)
+            print("u_global",u_global)
+
+            # Then we only get the solution if its lq is smaller than u_global
+            if lq < u_global:
+                # also update u_global if uq is smaller
+                if uq < u_global:
+                    u_global = uq
+                
+                temp_P.append(p.left)
+
+        #temp_right = p.data[:]
+        #temp_right[n] = 1
+        #p.right = Node(temp_right)
+        #temp_P.append(p.right)
 
     P = temp_P
     n += 1
@@ -163,7 +195,14 @@ for p in P:
     print(p.data)
 
 
+'''data_test = [0,1,None,None]
+CSF = data_test[:2]
 
+for i in range(0,2):
+    CSF[i] = CSF[i] * items[i].value
+
+print("CSF: ",CSF)
+print("Sum of CSF ", sum(CSF))'''
 
 
 
